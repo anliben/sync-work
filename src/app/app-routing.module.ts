1c9ad486d0442f3f121519dd4733bf1f47060c82
anliben <anliben.yayoi@gmail.com>
2024-01-04 21:31:26 -0300

    :sparkles: implementa page login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./core/home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
